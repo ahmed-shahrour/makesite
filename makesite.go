5989198651c9ade768ca0fb1547f0f5f06260183
Ahmed Shahrour <as.ahmed.shahrour@gmail.com>
2022-02-02 21:42:58 -0800

    v1.0 - Latest Post

--- makesite.go
+++ makesite.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"io/ioutil"
 	"os"
@@ -14,16 +15,20 @@ type Page struct {
 }
 
 func main() {
-	fileContents, _ := ioutil.ReadFile("first-post.txt")
+	fileFlag := flag.String("file", "first-post.txt", "Enter the name of the file to be converted")
+	flag.Parse()
+
+	fileContents, _ := ioutil.ReadFile(*fileFlag)
+	fileName := (*fileFlag)[:len(*fileFlag)-4]
 
 	page := Page{
 		TextFilePath: "./",
-		TextFileName: "first-post",
-		HTMLPagePath: "first-post.html",
+		TextFileName: fileName,
+		HTMLPagePath: fileName + ".html",
 		Content:      string(fileContents),
 	}
 
 	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
-	newFile, _ := os.Create("first-post.html")
+	newFile, _ := os.Create(page.HTMLPagePath)
 	t.Execute(newFile, page)
 }

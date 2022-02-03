ad107556e98036c52cf205a48a8089452e74cbe7
Ahmed Shahrour <as.ahmed.shahrour@gmail.com>
2022-02-02 21:12:23 -0800

    v1.0 - First Post

--- makesite.go
+++ makesite.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"io/ioutil"
+	"os"
+)
+
+type Page struct {
+	TextFilePath string
+	TextFileName string
+	HTMLPagePath string
+	Content      string
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	fileContents, _ := ioutil.ReadFile("first-post.txt")
+
+	page := Page{
+		TextFilePath: "./",
+		TextFileName: "first-post",
+		HTMLPagePath: "first-post.html",
+		Content:      string(fileContents),
+	}
+
+	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+	newFile, _ := os.Create("first-post.html")
+	t.Execute(newFile, page)
 }
